691d459ff966400f590991f99e140abfcac4581e
IconicLuck <uhasib959@gmail.com>
2023-05-27 14:27:42 +0600

    created two routes

---
+++ src/Pages/Login/Login.jsx
@@ -0,0 +1,11 @@
+
+
+const Login = () => {
+    return (
+        <div>
+            <h1 className="text-center text-4xl">This is Login component</h1>
+        </div>
+    );
+};
+
+export default Login;
